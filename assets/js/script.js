8f16a04a87ef16e8872616fac5b5943f3607d2cf
nigaraliyevas <naliyeva965@gmail.com>
2024-04-13 01:55:12 +0400

    some sections about to ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,6 +110,42 @@ $(".responsive").slick({
     // instead of a settings object
   ],
 });
+// main product slider-6
+$(".responsive-6").slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+// change slider butttons style
 const deals = document.querySelectorAll(".deals");
 deals.forEach((deal) => {
   const icon = deal.querySelectorAll(".slick-arrow");
@@ -117,3 +153,34 @@ deals.forEach((deal) => {
     i.classList.add("slider-buttons");
   });
 });
+//
+let lastClicked = null;
+const filterCars = document.querySelectorAll(".cars-body__left-item");
+filterCars.forEach((brands) => {
+  brands.addEventListener("click", () => {
+    if (lastClicked != null) {
+      lastClicked.style.backgroundColor = "#f6f9fc";
+      lastClicked.style.boxShadow = "none";
+    }
+    brands.style.backgroundColor = "white";
+    brands.style.boxShadow = "rgba(0, 0, 0, 0.05) 0px 6px 26px";
+    lastClicked = brands;
+  });
+});
+const filterBtns = document.querySelectorAll(".filter");
+const shops = document.querySelectorAll(".shops");
+const brands = document.querySelectorAll(".brands");
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const target = btn.innerHTML.trim();
+    if (target === "Brands") {
+      brands.forEach((brand) => brand.classList.remove("hidden"));
+      shops.forEach((shop) => shop.classList.add("hidden"));
+    } else {
+      shops.forEach((shop) => shop.classList.remove("hidden"));
+      brands.forEach((brand) => brand.classList.add("hidden"));
+    }
+  });
+});
+
